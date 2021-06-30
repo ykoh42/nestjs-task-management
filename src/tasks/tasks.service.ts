d34a60db696dc409f059b2252cc6c0f46c79eb17
Ykoh <ykoh@student.42seoul.kr>
2021-07-01 01:18:54 +0900

    refactor: createTask using DTO
    We can only fix service when changing the shape of createTaskDto.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -9,11 +10,12 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
-      title: title,
-      description: description,
+      title,
+      description,
       status: TaskStatus.OPEN,
     };
     this.tasks.push(task);
